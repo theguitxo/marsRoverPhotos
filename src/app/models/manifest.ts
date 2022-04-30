f6ee7bcbd3a8a923c9b5f1d30c0f3b244d5abfc6
Amador (Guitxo) <guitxo@guitxo.com>
2022-04-30 11:59:16 +0200

    primer commit

---
+++ src/app/models/manifest.ts
@@ -0,0 +1,19 @@
+import { CAMERA, STATUS } from "./constants";
+
+export interface ManifestPhoto {
+  sol: number;
+  earth_date: string;
+  total_photos: number;
+  cameras: CAMERA[];
+}
+
+export interface Manifest {
+  name: string;
+  landing_date: string;
+  launch_date: string;
+  status: STATUS;
+  max_sol: number;
+  max_date: string;
+  total_photos: number;
+  photos: ManifestPhoto[];
+}
